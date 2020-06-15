60cf15e750482a3b9f535760580ddec1a071c4ca
unknown <michael.messina@hotmail.fr>
2020-06-15 15:43:48 +0200

    fin du cours

--- src/build/partSelecter.vue
+++ src/build/partSelecter.vue
@@ -11,7 +11,10 @@
     </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin="{ bottom: '10px', right: '50px'}"
+     class="sale" v-show="selectedPart.onSale">
+     Sale!
+     </span>
   </div>
 </template>
 
@@ -86,9 +89,6 @@ export default {
   border: 3px solid #aaa;
 }
 .sale {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
   color: white;
   background-color: red;
   padding: 3px;
--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -18,7 +18,7 @@
             {{robot.head.title}}
           </td>
           <td class="cost">
-            {{robot.cost}}
+            {{robot.cost | currency}}
           </td>
         </tr>
       </tbody>
@@ -41,7 +41,7 @@
             {{robot.head.title}}
           </td>
           <td class="cost">
-            {{robot.cost}}
+            {{robot.cost | currency}}
           </td>
         </tr>
       </tbody>
@@ -50,8 +50,11 @@
 </template>
 
 <script>
+import currencyFilter from '../shared/currency-filter';
+
 export default {
   name: 'Cart',
+  filters: { currency: currencyFilter }, // on peut le declarer globallement comme les directives
   computed: {
     cart() {
       return this.$store.state.robots.cart;
